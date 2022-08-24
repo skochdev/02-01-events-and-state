6e05b0582ee8e520478d130edc5efc2fc6ef78c5
Bohdan Skochii <b.skochii@gmail.com>
2022-08-25 00:02:09 +0300

    now able to add new todos, along with checking todos as completed and deletings. Completed todos changes font color

---
+++ src/components/TodoAdder/TodoAdder.js
@@ -0,0 +1,40 @@
+import { Box } from '../Box';
+import { Component } from 'react';
+
+export class TodoAdder extends Component {
+  state = {
+    newTodoText: '',
+  };
+
+  onNewTodoChange = e => {
+    this.setState({ newTodoText: e.currentTarget.value });
+  };
+
+  handleSubmitNewTodo = e => {
+    e.preventDefault();
+    this.props.onSubmitNewTodo(this.state.newTodoText);
+    this.resetState();
+  };
+
+  resetState = () => {
+    this.setState({ newTodoText: '' });
+  };
+
+  render() {
+    const { handleSubmitNewTodo, onNewTodoChange } = this;
+    return (
+      <Box display="flex" justifyContent="center">
+        <form onSubmit={handleSubmitNewTodo}>
+          <label>
+            <textarea
+              onChange={onNewTodoChange}
+              value={this.state.newTodoText}></textarea>
+          </label>
+          <Box type="submit" as="button" display="block" mx="auto">
+            Add todo
+          </Box>
+        </form>
+      </Box>
+    );
+  }
+}
